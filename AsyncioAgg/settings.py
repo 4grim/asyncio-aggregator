d77ce3770ffc5d4cc6479316ca736b8ba5babc6a
4grim <amber.grimaldi@gmail.com>
2014-05-01 21:18:27 -0700

    change template debug to false

--- AsyncioAgg/settings.py
+++ AsyncioAgg/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
